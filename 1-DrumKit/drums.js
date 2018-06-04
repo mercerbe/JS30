c2e733edc662c5d59364bbe217f7bba90be93c1b
Admin <mercerbe@gmail.com>
2018-06-04 08:29:32 -0400

    js done, need to update mobile responsiveness

--- 1-DrumKit/drums.js
+++ 1-DrumKit/drums.js
@@ -1,4 +1,23 @@
-window.addEventListener('keydown', function(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keycode }"]`)
+$.backstretch('imgs/background.jpeg');
 
-});
+function playSound(event){
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  //audio
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  //key animations
+  key.classList.add('playing');
+}
+
+function removeTransition(event){
+  if(event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
